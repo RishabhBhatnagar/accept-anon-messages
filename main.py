76aad40e18490591a7701b49eacf739fffda1ee2
Rishabh Bhatnagar <rishabh.bhatnagar@xebia.com>
2025-02-06 12:21:15 +0530

    Keep a list of past requests
    Signed-off-by: Rishabh Bhatnagar <rishabh.bhatnagar@xebia.com>

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from flask import Flask, request, redirect, url_for
+from datetime import datetime
+from typing import List, Dict
+
 
 app = Flask(__name__)
+past_successful_requests: List[Dict] = []
+
+
+@app.get("/requests")
+def return_past_requests():
+    return past_successful_requests
 
 
 @app.get("/echo")
@@ -8,6 +17,10 @@ def echo():
     content = request.args.get('q')
     if content is None:
         return dict(error="Param 'q' must be set")
+    past_successful_requests.append({
+        "time": datetime.now(),
+        "query": content
+    })
     return dict(output=content)
 
 
@@ -18,3 +31,4 @@ def default():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
