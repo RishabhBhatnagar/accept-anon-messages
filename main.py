dd77395b8814d6495cc215972c4e2c12e9350b2d
Rishabh Bhatnagar <rishabh.bhatnagar@xebia.com>
2025-02-06 11:56:52 +0530

    Add a default redirect in the / handler
    Signed-off-by: Rishabh Bhatnagar <rishabh.bhatnagar@xebia.com>

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -9,6 +9,11 @@ def echo():
     if content is None:
         return dict(error="Param 'q' must be set")
     return dict(output=content)
+
+
+@app.get('/')
+def default():
+    return redirect(url_for("echo", **request.args))
 
 
 if __name__ == "__main__":
